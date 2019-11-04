0e3c929f2e8c2734ce7f915acd3ac98756b2885f
Saint Elder <30008225@student.toiohomai.ac.nz>
2019-11-04 18:46:17 +1300

    Unit Testing

---
+++ BaseProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BaseProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        public int Fname { get; set; }
+        public int Lname { get; set; }
+        public int Age { get; set; }
+
+
+        public Person (string f, string l)
+        {
+            Fname = f;
+            Lname = l;
+       
+        }
+
+        public string Greeting()
+        {
+            return $"Hello {Fname} {Lname}";
+        }
+
+
+        public Person1(string f, string l, string a)
+        {
+            Fname = f;
+            Lname = l;
+            Age = a;
+
+        }
+
+        public string Age()
+        {
+            return $"{Age} + 1";
+        }
+
+    }
+    
+}
